5806d4b2a10331fef4dcde47b7b5770ab681549d
smartbison <121305911+smartbison@users.noreply.github.com>
2023-07-16 16:37:53 -0400

    Initial Commit

---
+++ wordHunt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  wordHunt
+//
+//  Created by Dmitry Pavlov  on 7/16/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
